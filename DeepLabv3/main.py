32a4bbba0f8b3a02401f6313f1cb381b798ea95d
Nguyen Dinh Son <nguyendinhson@kaist.ac.kr>
2023-03-16 14:59:00 +0900

    Update main.py: - support input arguments - change to use custom dataset class to support "trainaug" dataset - wrap all in to main function

--- DeepLabv3/main.py
+++ DeepLabv3/main.py
@@ -1,63 +1,117 @@
 import torch 
 import torchvision
 from torch.utils.data import DataLoader, Subset
-import matplotlib.pyplot as plt
+
+from dataset import VOCSegmentation
 from utils.custom_transform import *
 from utils.solver import Solver
 from modules.layers import *
+import argparse
+import sys
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-print("Using",device)
+    parser.add_argument('-lr', '--learning-rate', type=float, default=1e-3,
+        help='the learning rate')
+    parser.add_argument('-e', '--num-epochs', type=int, default=5, 
+        help='the number of epochs')
+    parser.add_argument('-b', '--batch-size', type=int, default=8,
+        help='the batch size for training and validation')
+    parser.add_argument('--use-subset', action='store_true',
+        help='use subset to sanity check')
+    parser.add_argument('--trainaug', action='store_true', 
+        help='use trainaug dataset for training')
+    parser.add_argument('--gpu-id', type=int, default=0,
+        help='select gpu if there are many gpus, be sure id is valid')
+    parser.add_argument('--log-every', type=int, default=25,
+        help='print and log loss info at every iter')
+    parser.add_argument('-v', '--verbose', action='store_true', 
+        help='print info')
 
-torch.random.manual_seed(230)
+    args = parser.parse_args()
 
-# create a color pallette, selecting a color for each class
-palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
-colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
-colors = (colors % 255).numpy().astype("uint8")
-
-# prepare dataset
-DATASET_PATH = 'dataset/'
-
-class_names = ['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
-                'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 
-                'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+    return args
 
 
-train_preprocess = CoCompose([
-    CoRandomResized(scale=(0.5, 2.0)),
-    CoRandomFlip,
-    CoRandomCrop,
-    CoToTensor,
-    CoNormalize
-])
+def main():
+    # get input arguments
+    args = parse_args()
 
-val_preprocess = CoCompose([
-    CoCenterCrop,
-    CoToTensor,
-    CoNormalize
-])
+    device = torch.device('cuda:'+str(args.gpu_id) if torch.cuda.is_available() else 'cpu')
 
-mask_dataset = range(10*4)
+    print("Using",device)
 
-train_dataset = torchvision.datasets.VOCSegmentation(root=DATASET_PATH+'train', image_set="train", download=False, transforms=train_preprocess)
-sub_train_dataset = Subset(train_dataset, mask_dataset)
-train_loader = DataLoader(sub_train_dataset, batch_size=4, shuffle=False)
+    torch.random.manual_seed(230)
 
-val_dataset = torchvision.datasets.VOCSegmentation(root=DATASET_PATH+'val', image_set="val", download=False, transforms=val_preprocess)
-sub_val_dataset = Subset(val_dataset, mask_dataset)
-val_loader = DataLoader(sub_val_dataset, batch_size=4, shuffle=False)
+    # create a color pallette, selecting a color for each class
+    palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+    colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
+    colors = (colors % 255).numpy().astype("uint8")
 
-deeplabv3_16 = ResNet50_DeepLabV3_16(num_classes=21)
+    # prepare dataset
+    DATASET_PATH = 'dataset/'
 
-solver = Solver(
-    deeplabv3_16, 
-    train_loader, 
-    val_loader, 
-    num_classes=21,
-    num_epochs=20, 
-    lr=0.0005, 
-    device=device)
+    class_names = ['background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+                    'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 
+                    'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
 
-solver.train()
+
+    train_preprocess = CoCompose([
+        CoRandomResized(scale=(0.5, 2.0)),
+        CoRandomFlip,
+        CoRandomCrop,
+        CoToTensor,
+        CoNormalize
+    ])
+
+    val_preprocess = CoCompose([
+        CoCenterCrop,
+        CoToTensor,
+        CoNormalize
+    ])
+
+    mask_dataset = range(100)
+
+    # full_train_dataset = torchvision.datasets.VOCSegmentation(root=DATASET_PATH+'train', image_set="train", download=False, transforms=train_preprocess)
+
+    if args.trainaug:
+        full_train_dataset = VOCSegmentation(root=DATASET_PATH+'train', year="2012_aug", image_set="train", download=False, transforms=train_preprocess)
+    else:
+        full_train_dataset = VOCSegmentation(root=DATASET_PATH+'train', image_set="train", download=False, transforms=train_preprocess)
+    sub_train_dataset = Subset(full_train_dataset, mask_dataset)
+
+    # sys.exit(0)
+
+    # full_val_dataset = torchvision.datasets.VOCSegmentation(root=DATASET_PATH+'val', image_set="val", download=False, transforms=val_preprocess)
+    full_val_dataset = VOCSegmentation(root=DATASET_PATH+'val', image_set="val", download=False, transforms=val_preprocess)
+    sub_val_dataset = Subset(full_val_dataset, mask_dataset)
+
+    if args.use_subset:
+        train_dataset = sub_train_dataset
+        val_dataset = sub_val_dataset
+    else:
+        train_dataset = full_train_dataset
+        val_dataset = full_val_dataset
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+
+    # train model
+    deeplabv3_16 = ResNet50_DeepLabV3_16(num_classes=21)
+
+    solver = Solver(
+        deeplabv3_16, 
+        train_loader, 
+        val_loader, 
+        num_classes=21,
+        num_epochs=args.num_epochs, 
+        lr=args.learning_rate, 
+        device=device,
+        log_every=args.log_every,
+        verbose=args.verbose)
+
+    solver.train()
+
+if __name__ == '__main__':
+    main()
